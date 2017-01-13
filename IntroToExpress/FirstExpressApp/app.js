d937d55b642174cc3ba07988263031eb445281ad
kovacssandorUI <sandorkovacs122@gmail.com>
2017-01-14 00:04:21 +0100

    start_express

---
+++ IntroToExpress/FirstExpressApp/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by sa on 2017.01.13..
+ */
+console.log("OUR EXPRESS APP WILL GO HERE!");
+var express =require("express");
+var app =express();
+
+// "/" => "Hi there!"
+app.get("/", function (req, res) {
+    res.send("Hi there!");
+});
+
+// "/bye"=> "Goodbye!"
+// "/dog"=> "MEOW!"
+
+app.listen(3000, function () {
+    console.log("Server has started!!");
+})
